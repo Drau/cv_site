f15221bf73cbd5fc1df1edcba39228e795d94ea6
Michael <michreyfman@gmail.com>
2017-03-08 23:39:04 +0200

    redirect

--- cvs/urls.py
+++ cvs/urls.py
@@ -17,8 +17,11 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.views.generic.base import TemplateView
+from django.views.generic.base import RedirectView
+
 
 urlpatterns = [
+    url(r'^$', RedirectView.as_view(url='cvs/', permanent=False), name='base'),
     url(r'^cvs/', include('cvr.urls')),
     url(r'^admin/', admin.site.urls),
 ]
